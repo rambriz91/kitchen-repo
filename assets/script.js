0bdd38557e3b285e82db1ba12e48193a4ff7a5bf
rambriz91 <rambriz91@gmail.com>
2023-08-10 20:06:34 -0700

    created a function for the add button.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
+var addBtnEl = document.getElementById('addBtn');
+var clearBtnEl = document.getElementById('clearList');
+var submitBtnEl = document.getElementById('submit');
+var userInput = document.getElementById('userInput');
+var ingList = document.getElementById('ingList')
+
 var APIkey = '1e8651b92cc9448eb4ff9216c557959f';
+
+var ingArr = [];
 
 // fetch request for the spoon APIkey. userInput var is a placeholder 
 function getRecipe() {
-    userInput= ['apple', 'feta cheese', 'olive oil'];
-    let spoonAPI = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=' + APIkey + '&ingredients=' + userInput + '&number=5';
+    ingArr= ['chicken', 'feta cheese', 'olive oil'];
+    let spoonAPI = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=' + APIkey + '&ingredients=' + ingArr + '&number=5';
     
     fetch(spoonAPI)
         .then(function (response) {
@@ -13,3 +21,17 @@ function getRecipe() {
             console.log(data);
         })
 };
+addBtnEl.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (ingArr.includes(userInput.value) || userInput.value === "") {
+        userInput.value = '';
+        return;
+    }
+    ingArr.push(userInput.value);
+    var ingredient = document.createElement('li');
+    ingredient.textContent = userInput.value;
+    ingList.appendChild(ingredient);
+    userInput.value = '';
+});
+
+submitBtnEl.addEventListener('click', getRecipe);
